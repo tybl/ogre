40377c432656e8233eab348289383cf11065a09b
Timothy Lyons <t.brandon.lyons@gmail.com>
2020-05-29 23:28:28 +0000

    flushed out some structure

---
+++ src/tasklib/util/uuid.hpp
@@ -0,0 +1,48 @@
+/*******************************************************************************
+ * Copyright (c) 2020, Timothy Lyons
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+ * REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+ * INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+ * LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+ * OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ * PERFORMANCE OF THIS SOFTWARE.
+ *
+ * https://opensource.org/licenses/ISC
+ ******************************************************************************/
+#pragma once
+
+#include <string>
+
+namespace vodka::util {
+
+class uuid {
+  std::string mValue;
+
+public:
+  uuid();
+
+  static auto Create() -> uuid;
+
+  auto operator<=>(uuid const&) const = default;
+
+  [[nodiscard]]
+  auto to_string() const -> std::string const&;
+
+private:
+  static constexpr const auto uuid_file = "/proc/sys/kernel/random/uuid";
+
+  static constexpr const auto nil = "00000000-0000-0000-0000-000000000000";
+
+  explicit uuid(std::string value);
+
+  static auto GenerateStr() -> std::string;
+
+}; // struct Uuid
+
+} // namespace vodka::util
