a1f5b8b205827fc0dac9485d4c0678c31ad2f45e
Timothy Lyons <t.brandon.lyons@gmail.com>
2020-06-08 19:28:49 +0000

    Removed legal syntax breaking cppcheck

--- apps/task/main.cpp
+++ apps/task/main.cpp
@@ -20,11 +20,12 @@
 
 #include <iostream>
 
-auto main(int argc, char const* argv[]) -> int
-try {
-  ogre::Application app(PROJECT_NAME, PROJECT_VERSION);
-  return app.run(argc, argv);
-} catch (std::exception& err) {
-  std::cerr << "Error: " << err.what() << std::endl;
+auto main(int argc, char const* argv[]) -> int {
+  try {
+    ogre::Application app(PROJECT_NAME, PROJECT_VERSION);
+    return app.run(argc, argv);
+  } catch (std::exception& err) {
+    std::cerr << "Error: " << err.what() << std::endl;
+  }
   return -1;
 }
