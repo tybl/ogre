0bf93db84ce9afdca272e417524adcbbad5934ca
Brandon Lyons <t.brandon.lyons@gmail.com>
2023-07-31 20:46:34 -0400

    #7: Use consistent formatting

--- libs/libcli/test/cli/Command.cpp
+++ libs/libcli/test/cli/Command.cpp
@@ -14,9 +14,10 @@ TEST_CASE("tasklib/cli/Command") {
   int result = rand();
   //  Command
   ogre::Command cmd("git");
-  cmd.add_action([result](ogre::Parameters const& /*args*/) -> int { return result; });
+  cmd.add_action(
+      [result](ogre::Parameters const& /*args*/) -> int { return result; });
   //  Input
-  std::vector<std::string_view> input { "git" };
+  std::vector<std::string_view> input{"git"};
 
   // Execute
   CHECK(result == cmd.run(input));
@@ -33,7 +34,7 @@ TEST_CASE("tasklib/cli/Command subcommand") {
     return result;
   });
   //  Input
-  std::vector<std::string_view> input { "git", "add", "main.cpp" };
+  std::vector<std::string_view> input{"git", "add", "main.cpp"};
 
   // Execute
   CHECK(result == cmd.run(input));
@@ -45,10 +46,9 @@ TEST_CASE("tasklib/cli/Command option") {
   //  Command
   ogre::Command cmd("git");
   auto& rm = cmd.add_subcommand("rm");
-  rm.add_option("-f", "--force")
-    .add_help("Override the up-to-date check.");
-  rm.add_option("-r")
-    .add_help("Allow recursive removal when leading directory name is given.");
+  rm.add_option("-f", "--force").add_help("Override the up-to-date check.");
+  rm.add_option("-r").add_help(
+      "Allow recursive removal when leading directory name is given.");
   rm.add_action([result](ogre::Parameters const& args) -> int {
     REQUIRE(args.Options.contains("-f"));
     CHECK(args.Options.at("-f") == "true");
@@ -59,7 +59,7 @@ TEST_CASE("tasklib/cli/Command option") {
     return result;
   });
   //  Input
-  std::vector<std::string_view> input { "git", "rm", "-f", "main.cpp" };
+  std::vector<std::string_view> input{"git", "rm", "-f", "main.cpp"};
 
   // Execute
   CHECK(result == cmd.run(input));
